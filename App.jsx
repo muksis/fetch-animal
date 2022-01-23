da1ad570017c1d59e8625ef58437b9477dec99db
muksis <60219890+muksis@users.noreply.github.com>
2022-01-23 18:46:12 -0500

    first commit

---
+++ App.jsx
@@ -0,0 +1,31 @@
+function App() {
+  const {useState, useEffect} = React;
+  const [image, setImage] = useState([]);
+  const [name, setName] = useState("");
+  const [habitat, setHabitat] = useState("");
+  const [diet, setDiet] = useState("");
+
+  const fetchData = () => {
+      fetch ("https://zoo-animal-api.herokuapp.com/animals/rand")
+      .then(response => response.json())
+      .then(data => {
+        setImage(data.image_link),
+        setName(data.name),
+        setHabitat(data.habitat),
+        setDiet(data.diet)
+      })
+  };
+  useEffect(fetchData, []);
+
+  return (
+    <div> 
+      <img src={image} />
+      <h2>Name: {name}</h2>
+      <p>Habitat: {habitat}</p>
+      <p>Diet: {diet}</p>
+    </div>
+  );
+}
+
+// ========================================
+ReactDOM.render(<App />, document.getElementById("root"));
